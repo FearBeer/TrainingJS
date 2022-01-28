d15e74f1c885ee55a007cb64fa9d65f8c8d16ce2
Ruslan <korus86@mail.ru>
2022-01-28 13:00:26 +0300

    + some cycles

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let money = prompt("Your budget on month: ");
+let money = +prompt("Your budget on month: ");
 let time = prompt("Enter the date in the format YYYY-MM-DD: ");
 
 const appData = {
@@ -16,12 +16,26 @@ const appData = {
     savings: false,
 }
 
-let answer1 = prompt("Enter a required expense item for this month : "),
-    answer2 = prompt("How much it will cost? "),
-    answer3 = prompt("Enter a required expense item for this month : "),
-    answer4 = prompt("How much it will cost? ");
+for (let i = 0; i < 2; i++) {
+    let answer1 = prompt("Enter a required expense item for this month : "),
+        answer2 = prompt("How much it will cost? ");
+    if (typeof (answer1) !== null && typeof (answer2) !== null && answer1 !== '' && answer2 !== '' && answer1.length < 50 && answer2.length < 50) {
+        appData.expenses[answer1] = answer2;
+    } else {
+        console.log ("bad result");
+        i--;
+    }
+}
 
-appData.expenses.answer1 = answer2;
-appData.expenses.answer3 = answer4;
+appData.moneyPerDay = appData.budget / 30
+alert("Everyday budget: ", appData.moneyPerDay);
 
-alert(appData.budget / 30);
+if (appData.moneyPerDay < 500) {
+    console.log("Need more gold!!!");
+} else if (appData.moneyPerDay >= 500 && appData.moneyPerDay < 1500) {
+    console.log("Need some gold...");
+} else if (appData.moneyPerDay >= 1500) {
+    console.log("Could you borrow me some coins?");
+} else {
+    console.log("Something is wrong...");
+}
