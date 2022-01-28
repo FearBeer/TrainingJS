447ae92e6827b39cf1b6a85b1d388ba5c9ae39a5
Ruslan <korus86@mail.ru>
2022-01-29 01:57:28 +0300

    add main.js(only get all elements)

---
+++ budget/js/main.js
@@ -0,0 +1,24 @@
+let startButton = document.getElementById('start');
+
+let budgetValue = document.querySelector('budget-value');
+let daybudgetValue = document.querySelector('daybudget-value');
+let levelValue = document.querySelector('level-value');
+let expensesValue = document.querySelector('expenses-value');
+let optionalExpensesValue = document.querySelector('optionalexpenses-value');
+let incomeValue = document.querySelector('income-value');
+let monthSavingsValue = document.querySelector('monthsavings-value');
+let yearSavingsValue = document.querySelector('yearsavings-value');
+
+let expensesItem = document.querySelectorAll('.expenses-item');
+let expenseButton = document.getElementsByTagName('button')[0];
+let optionalExpensesButton = document.getElementsByTagName('button')[1];
+let countBudgetButton = document.getElementsByTagName('button')[2];
+
+let incomeItem = document.querySelector('.choose-income');
+let checkSavings = document.querySelector('#savings');
+let sumValue = document.querySelector('.choose-sum');
+let percentValue = document.querySelector('.choose-percent');
+let yearValue = document.querySelector('.year-value');
+let monthValue = document.querySelector('.month-value');
+let dayValue = document.querySelector('.day-value');
+
